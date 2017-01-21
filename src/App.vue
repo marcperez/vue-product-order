a9a50912f2dacafeade1c2ec6768a960c3ce0a8a
Marc Perez <marcperez2000@gmail.com>
2017-01-21 22:33:33 +0100

    first import

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+  $icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/";
+  @import "~bootstrap-sass/assets/stylesheets/bootstrap";
+</style>
